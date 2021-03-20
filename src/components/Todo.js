7fa07fa29d61d113b06ad74377c698f2820f78d9
tushardahiya <tushardahiya84@gmail.com>
2021-03-20 15:16:13 +0530

    tried out useEffect hook by getting all initial todos

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,23 @@
-import React, {useState} from 'react';
+import React, {useState , useEffect} from 'react';
 import axios from 'axios';
 
 const Todo = props => {
 
     const [todoName, setTodoName ] = useState('');
     const [todoList, setTodoList] = useState([]);
+
+    useEffect( () => {
+        axios.get('https://hooks-practice-58c18-default-rtdb.firebaseio.com/todos.json')
+        .then( res => {
+            console.log(res.data);
+            const todoData = res.data;
+            const todos=[];
+            for(const key in todoData){
+                todos.push( {id:key , name:todoData[key].name});
+            }
+            setTodoList(todos);
+        } )
+    })
 
     const inputChangeHandler = (event) => {
         setTodoName(event.target.value);
@@ -31,7 +44,7 @@ const Todo = props => {
         <button type="button" onClick={todoAddHandler}>Add</button>
         <ul>
             {todoList.map(todo => (
-            <li key={todo}>{todo}</li>
+            <li key={todo.id}>{todo.name}</li>
             ))}
         </ul>
     </React.Fragment>
