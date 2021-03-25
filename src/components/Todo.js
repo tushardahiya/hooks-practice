64022eb5f796c48de87e43150980e30c0dc3ea5b
tushardahiya <tushardahiya84@gmail.com>
2021-03-25 17:02:53 +0530

    tried cleanup feature in useEffect

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -16,12 +16,30 @@ const Todo = props => {
                 todos.push( {id:key , name:todoData[key].name});
             }
             setTodoList(todos);
-        } )
-    }, [])
+        } );
+        // return statement kinda works like componentdidUnmount 
+        //i.e return function will runs during cleanup which can be specified in the second arguement
+        return ()=> {
+            console.log('cleanup');
+        };
+    }, []);
     // the second arguement to useEffect controls on what changes the useeffect will run.....
     //all the values in second argument are checked if they changed and if so only then useeffect runs
 
     // sending an empty [] as the second arguement works just like componentdidMount i.e it will only run once when the component mounts
+    
+    // ANOTHER useeffect as an example which logs the mouse coordinates
+    // but without the return statement it starts running many loggers when we for example start typing a todo
+    const mouseMoveHandler = (event) => {
+        console.log(event.clientX,event.clientY);
+    }
+    
+    useEffect( () => {
+        document.addEventListener('mousemove', mouseMoveHandler);
+        return () =>{
+            document.removeEventListener('mousemove', mouseMoveHandler);
+        } 
+    }, []);
 
     const inputChangeHandler = (event) => {
         setTodoName(event.target.value);
